e0dde9ba47d49162f998ea968dffae092f481ddd
GOVIND RATNAKAR NAIK <57467998+govindrnaik@users.noreply.github.com>
2021-02-07 22:57:44 +0530

    Create Assignment No. 1 Q1.py

---
+++ Assignment No. 1 Q1.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+print("Enter which operation would you like to perform?")
+ch = input("Enter any of these opeartion +,-,*,/: ")
+
+result = 0
+if ch == '+':
+    result = num1 + num2
+elif ch == '-':
+    result = num1 - num2
+elif ch == '*':
+    result = num1 * num2
+elif ch == '/':
+    result = num1 / num2
+else:
+    print("undefined characters")
+
+print(num1, ch , num2, ":", result)
